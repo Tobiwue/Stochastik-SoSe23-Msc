12e7a1d8bd837e80df1040e60191d447af8ca87e
Tobiawue <s0573300@htw-berlin.de>
2023-06-21 17:57:54 +0200

    Commit-Beschreibung

---
+++ server.R
@@ -0,0 +1,62 @@
+# This is the server logic of a Shiny web application.
+
+library(shiny)
+library(ggplot2)
+library(shinyWidgets)
+library(grid)
+
+# read miete03 (provided by the lecturer and is unchanged)
+dataset <- read.csv('C:/Users/Tobia/Documents/Stochastik/data/housing.csv',
+                    header = TRUE)
+
+# Define server logic required to draw a histogram
+function(input, output, session) {
+  
+  # calc for gaussian distribution
+  calculate_normal <- reactive({
+    range_start <- input$threshold_range[1]
+    range_end <- input$threshold_range[2]
+    filtered_data <- subset(dataset, dataset$households >= range_start & dataset$households <= range_end)
+    mean_value <- mean(filtered_data$households)
+    sd_value <- sd(filtered_data$households)
+    x <- seq(range_start, range_end, length.out = 100)
+    y <- dnorm(x, mean = mean_value, sd = sd_value)
+    data.frame(x = x, y = y)
+  })
+  
+  # meta data
+  output$summary_text <- renderText({
+    range_start <- input$threshold_range[1]
+    range_end <- input$threshold_range[2]
+    
+    filtered_data <- subset(dataset, dataset$households >= range_start & dataset$households <= range_end)
+    mean_value <- mean(filtered_data$households)
+    sd_value <- sd(filtered_data$households)
+    
+    paste(
+      " Range: [", range_start, "-", range_end, "]\n",
+      "Mean: ", round(mean_value, 2), "\n",
+      "SD:   ", round(sd_value, 2)
+    )
+  })
+  
+  # draw plot
+   output$gaussian_distribution_plot <- renderPlot({
+      range_start <- input$threshold_range[1]
+      range_end <- input$threshold_range[2]
+      filtered_data <- subset(dataset, dataset$households >= range_start & dataset$households <= range_end)
+      mean_value <- mean(filtered_data$households)
+      sd_value <- sd(filtered_data$households)
+    
+      mean_sd_plot<-ggplot() +
+      geom_histogram(data = filtered_data, aes(x = households), binwidth = 0.1, fill = "blue", color = "black") +
+      geom_line(data = calculate_normal(), aes(x = x, y = y), color = "red", size = 1) +
+      geom_vline(xintercept = range_start, linetype = "dashed", color = "green", size = 1) +
+      geom_vline(xintercept = range_end, linetype = "dashed", color = "green", size = 1) +
+      labs(x = "Values", y = "Frequenzy") +
+      theme_minimal()
+
+      print(mean_sd_plot)
+      print(mean_sd_plot)
+  })
+}
---
+++ ui.R
@@ -0,0 +1,49 @@
+# This is the user-interface definition of a Shiny web application.
+
+library(shiny)
+library(ggplot2)
+library(shinyWidgets)
+library(grid)
+
+# read miete03 (provided by the lecturer and is unchanged)
+dataset <- read.csv('C:/Users/Tobia/Documents/Stochastik/data/housing.csv',
+                    header = TRUE)
+
+# Define UI for application that draws a histogram
+fluidPage(
+  titlePanel("ShinyApp Stochastik SoSe23 - HTW Berlin FB4 MSC"),
+    tabsetPanel(
+      # Tab 1
+      tabPanel("Gaussian Distribution",
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput("threshold_range", "Scope", 
+                  #min = 0, max = max(dataset$households), value = c(0, max(dataset$households)),step = 1)
+                  min = 0, max = 500, value = c(0, 500),step = 1)
+            ),
+    mainPanel(
+      plotOutput("gaussian_distribution_plot"),
+      verbatimTextOutput("summary_text")
+         )
+      )),
+    # Tab 2
+    tabPanel("Tab 2",
+             sidebarLayout(
+               sidebarPanel(
+                 # Inhalt der Seitenleiste für Tab 2
+               ),
+               mainPanel(
+                 # Hauptinhalt für Tab 2
+               )
+             )),
+    # Tab 3
+    tabPanel("Tab 3",
+             sidebarLayout(
+               sidebarPanel(
+                 # Inhalt der Seitenleiste für Tab 3
+               ),
+               mainPanel(
+                 # Hauptinhalt für Tab 3
+               )
+             ))
+))
