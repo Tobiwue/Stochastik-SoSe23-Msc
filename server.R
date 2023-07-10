27555a51ca1700dc06914996dd7b3fe1dc14bda7
AliceKitchkin <52663353+AliceKitchkin@users.noreply.github.com>
2023-07-10 13:30:26 +0200

    Update server.R

--- server.R
+++ server.R
@@ -29,15 +29,15 @@ function(input, output, session) {
   #### distribution as reactive function ####
   # calc distribution as reactive function
   #calculate_normal <- reactive({
-    #range_start <- input$threshold_range[1]
-    #range_end <- input$threshold_range[2]
-    #filtered_data <- subset(housing, housing$households >= range_start & housing$households <= range_end)
-    #mean_value <- mean(filtered_data$households)
-    #sd_value <- sd(filtered_data$households)
-    #x <- seq(range_start, range_end, length.out = 100)
-   # y <- dnorm(x, mean = mean_value, sd = sd_value)
-    #data.frame(x = x, y = y)
- # })
+  #range_start <- input$threshold_range[1]
+  #range_end <- input$threshold_range[2]
+  #filtered_data <- subset(housing, housing$households >= range_start & housing$households <= range_end)
+  #mean_value <- mean(filtered_data$households)
+  #sd_value <- sd(filtered_data$households)
+  #x <- seq(range_start, range_end, length.out = 100)
+  # y <- dnorm(x, mean = mean_value, sd = sd_value)
+  #data.frame(x = x, y = y)
+  # })
   #### end ####
   
   #### metadata textbox ####
@@ -107,7 +107,7 @@ function(input, output, session) {
     ggplotly(density_plot)
   })
   #### end ####
-
+  
   #### get sample for interval as reactive function ####
   sample_data <- reactive({
     filtered_data <- subset(housing, housing$households <=2000 )
@@ -162,9 +162,9 @@ function(input, output, session) {
   createPlot <- function(data) {
     confidencePlot <- ggplot(data, aes(x = x, y = mean_value)) +
       geom_point(size = 3, aes(text = paste(" Mean: ", round(mean_value, 3), "<br>",
-                                             "Error: ", round(upper_bound - mean_value, 3), "<br>",
-                                             "Lower Bound: ", round(lower_bound, 3), "<br>",
-                                             "Upper Bound: ", round(upper_bound, 3)))) +
+                                            "Error: ", round(upper_bound - mean_value, 3), "<br>",
+                                            "Lower Bound: ", round(lower_bound, 3), "<br>",
+                                            "Upper Bound: ", round(upper_bound, 3)))) +
       geom_errorbar(aes(y = mean_value, ymin = lower_bound, ymax = upper_bound),
                     width = 0.4) +
       #coord_flip() +
@@ -178,7 +178,7 @@ function(input, output, session) {
   output$qqplot <- renderPlotly({
     filtered_data <- lapply(housing, head, 2000)
     qqdata <- as.data.frame(filtered_data[[input$column]])
-
+    
     #draw qqplot
     qqplot <- ggplot(qqdata, aes(sample = qqdata[, 1])) +geom_qq() +
       geom_qq_line() +
@@ -204,8 +204,60 @@ function(input, output, session) {
       sprintf("W-Value: %.4f\n", result$statistic),
       paste("P-Value:",result$p.value)
     )
-          
-    })
+    
+  })
   
+  #### end ####
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  output$h1_grenze <- renderText({ input$h0_grenze })
+  
+  
+  
+  #### Dataset updated as reactive ####
+  data_r <- reactive({
+    f_data <- subset(housing, housing$households >= input$hypo_range[1] & housing$households <= input$hypo_range[2])
+  })
+  #### end ####
+  
+  
+  #### T-Test as reactive ####
+  t_test_r <- reactive({  
+    data <- data_r()
+    data <- data$households
+    t_test <- t.test(data, mu = input$h0_grenze, alternative = "two.sided", conf.level = input$alpha)
+  })
+  #### end ####
+  
+  output$ttest <- renderText({ paste(t_test_r()) })
+  
+  
+  #### Hyppothesentest plot ####
+  output$hypothesentest_plot <- renderPlotly({
+    data <- data_r()
+    data <- data$households
+    
+    t_test <- t_test_r()
+    
+    hypo_plot <- ggplot() +
+      geom_bar(data = data_r(), aes(x = households), fill = "#C8DAEA", color = "#C8DAEA") +
+      geom_vline(xintercept = input$hypo_range[1], linetype = "solid", color = "#a3a3a3", size = 0.5) +
+      geom_vline(xintercept = input$hypo_range[2], linetype = "solid", color = "#a3a3a3", size = 0.5) +
+      geom_vline(xintercept = input$h0_grenze, linetype = "solid", color = "#428bca", size = 0.5) +
+      geom_vline(xintercept = t_test$conf.int, linetype = "dashed", color = "#428bca", size = 0.5) +
+      geom_text(aes(x = t_test$conf.int, y = 0, label = c("a/2", "1-(a/2)")), color = "black") + 
+      geom_text(aes(x = input$h0_grenze, y = 0, label = "H0 Grenze"), color = "black", y= 5) +
+      labs(x = "Values", y = "Frequency") +
+      theme_minimal()
+    ggplotly(hypo_plot)
+  })
   #### end ####
 }
